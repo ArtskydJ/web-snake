306216732aa2cd3da0060ee705a86e43efb935a2
Joseph Dykstra <josephdykstra@gmail.com>
2016-12-14 21:16:56 -0600

    Attempt to disable 300ms delay before click

--- src/controls/tap.js
+++ src/controls/tap.js
@@ -1,7 +1,10 @@
 var deadZone = 0.25
 
 module.exports = function listenForClickEvents(changeDirection) {
-	canvas.onmousedown = function (e) {
+	canvas.onmousedown = handleTap
+	canvas.ontouchend = handleTap
+
+	function handleTap(e) {
 		var leftRightAxis = (e.clientX / canvas.clientWidth * 2) - 1
 		var upDownAxis = (e.clientY / canvas.clientHeight * 2) - 1
 		var useLeftRightAxis = Math.abs(leftRightAxis) > Math.abs(upDownAxis)
